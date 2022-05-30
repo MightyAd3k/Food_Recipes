3692da548e16040495ed6aac997adcf5ae772804
Adrian Drewniak <adriandrewniak19@gmail.com>
2022-05-30 12:20:33 +0200

    displaying main page

--- bestrecipes/urls.py
+++ bestrecipes/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from food.views import IndexView, LandingPage
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test', IndexView.as_view()),
+    path('', LandingPage.as_view())
 ]
--- food/views.py
+++ food/views.py
@@ -9,3 +9,9 @@ class IndexView(View):
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
         return render(request, "test.html", ctx)
+
+
+class LandingPage(View):
+
+    def get(self, request):
+        return render(request, "index.html")
