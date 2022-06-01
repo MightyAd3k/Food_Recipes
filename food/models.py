622e2ff5b3d9f246f361250846796da0c1fc1991
Adrian Drewniak <adriandrewniak19@gmail.com>
2022-06-01 12:26:40 +0200

    added new models: Plan, DayName and RecipePlan

--- food/models.py
+++ food/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
 class Recipe(models.Model):
     name = models.CharField(max_length=200)
     ingredients = models.CharField(max_length=40)
@@ -14,3 +13,23 @@ class Recipe(models.Model):
 
     def ingredients_as_list(self):
         return self.ingredients.split()
+
+
+class Plan(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=150)
+    created = models.DateTimeField(auto_now=True, null=True, blank=True)
+    recipes = models.ManyToManyField(Recipe)
+
+
+class DayName(models.Model):
+    name = models.CharField(max_length=10)
+    order = models.IntegerField(unique=True)
+
+
+class RecipePlan(models.Model):
+    meal_name = models.CharField(max_length=10)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
+    order = models.IntegerField()
+    day_name = models.ForeignKey(DayName, on_delete=models.CASCADE)
